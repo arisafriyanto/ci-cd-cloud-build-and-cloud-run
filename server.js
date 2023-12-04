95564b78f7d9753c6afe334f4bf746bda95d2e96
arisafriyanto <arisafriyanto1933@gmail.com>
2023-12-04 22:07:01 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running`);
+});
