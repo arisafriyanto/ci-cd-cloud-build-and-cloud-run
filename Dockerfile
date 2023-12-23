229337924716b448b047e680d06dfb0440b90b12
arisafriyanto <arisafriyanto1933@gmail.com>
2023-12-23 09:54:46 +0700

    try

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:14.21.2-alpine
 WORKDIR /app
 COPY . .
 RUN npm install
-EXPOSE 5000
+EXPOSE 8000
 CMD [ "npm", "run", "start"]
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running`);
+    console.log(`Server is running on ${PORT}`);
 });
