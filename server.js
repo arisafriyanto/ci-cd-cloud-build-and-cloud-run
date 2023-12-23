cfb573da790644b6aa79b5ed3690533eaa1a4021
arisafriyanto <arisafriyanto1933@gmail.com>
2023-12-23 11:30:14 +0700

    add app name

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
+const APP_NAME = process.env.APP_NAME;
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World, Mr.Jacky!');
+    res.status(200).send(`Hello World, Mr.Jacky in the ${APP_NAME}`);
 });
 
 app.listen(PORT, () => {
